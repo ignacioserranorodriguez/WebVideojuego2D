df098e0e2d489111fb936d78b3a6b41d45a32985
Ignacio Serrano Rodriguez <ignacioserranorodriguez@gmail.com>
2024-03-06 19:42:18 +0100

    efectos de sonido funciona

--- src/js/tab.js
+++ src/js/tab.js
@@ -1,17 +1,17 @@
 'use strict'
 
 const li = document.querySelectorAll('.li')
-const bloque = document.querySelectorAll('.bloque')
+const tabs = document.querySelectorAll('.tabs')
 
 li.forEach( ( cadaLi , i )=>{
     li[i].addEventListener('click',()=>{
         li.forEach( ( cadaLi , i )=>{
             li[i].classList.remove('visible')
-            bloque[i].classList.remove('visible')
+            tabs[i].classList.remove('visible')
         })
 
         li[i].classList.add('visible')
-        bloque[i].classList.add('visible')
+        tabs[i].classList.add('visible')
 
     })
 })
